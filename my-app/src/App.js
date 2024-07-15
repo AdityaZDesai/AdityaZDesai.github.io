4e6164afc743849325a113587ff49d02d196705f
Aditya Desai <adityadesai753@gmail.com>
2024-07-15 16:30:26 +1000

    changed to https

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,7 +17,7 @@ import UpdateBlog from './UpdateBlog.js';
 
 function App() {
   const [message, setMessage] = useState('');
-  const url = "http://3.24.134.28:8000/"
+  const url = "https://3.24.134.28:8000/"
   useEffect(() => {fetch(url).then((res) => res.text())
     .then((data) => setMessage(data))
     .catch((err) => console.log(err));
